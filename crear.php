07a624e8f421d68cb8d967d9a08ed9b706c26a25
santy patiño <santyp638@gmail.com>
2020-03-03 20:29:44 -0500

    L4G03-formulario

---
+++ crear.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <link rel="icon"href="ico/Favicon.ico">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>login</title>
+</head>
+<body>
+ 
+ <div class="login-box">
+     
+     
+   <img src="img/lgo2.png" class="avatar" alt="Avatar Image">
+     <h1>Login</h1>
+     
+     <form action="guardar.php" method="post">
+         
+            <label for="usuario">Usuario</label> 
+            <input type="text" placeholder="usuario" name="nombre">
+            
+            <label for="email">Email</label> 
+            <input type="text" placeholder="email" name="email">
+            
+            <br>
+           <button type="submit"> Gruardar</button> 
+            
+            
+             
+         
+    </form>
+     
+ </div>
+ 
+
+    
+</body>
+</html>
---
+++ guardar.php
@@ -0,0 +1,41 @@
+Guardando la persona <br>
+<?php
+    //datos
+    $host ="localhost";
+    $user ="root";
+    $password=""; 
+    $db="base_persona";
+
+
+
+    //crear la conexion
+    $con=new mysqli($host , $user ,$password,$db);
+
+    //pueba conexion
+    if($con->connect_error){
+        echo "base de datos erroneo<br>";
+
+    }else{
+        echo "conectados a la base de datos <br>";
+    }
+
+    $nombre=$_POST["nombre"];
+    $email=$_POST["email"];
+
+        echo "la informacion enviada es :  nombre:$nombre email:$email";
+
+        $query= "insert into usuario(nombre,email) values('$nombre','$email')";
+
+    if($con->query($query)){//true or false
+
+            echo "<br>personas guardada correctamente";
+    }else{
+            echo "<br>no se ha podido guardar la persona <br>".$con->error;
+
+    }
+    
+
+    $con ->close();
+
+    header('Location: crear.php');
+?>
